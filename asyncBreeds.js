865e465e23229f4f0647eb86f53ae68e67250afc
justinmurphycalgary <justinmurphyis@gmail.com>
2022-02-16 18:21:41 -0700

    fixed 2

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,18 +1,16 @@
-console.clear()
-const fs = require('fs');
+console.clear();
+const fs = require("fs");
 
-const breedDetailsFromFile = function(breed) {
-  console.log('breedDetailsFromFile: Calling readFile...');
-  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log('data :>> ', data);
+const breedDetailsFromFile = function (breed) {
+  console.log("breedDetailsFromFile: Calling readFile...");
+  fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
+    console.log("data :>> ", data);
     //console.log("In readFile's Callback: it has the data.");
-   
+
     if (!error) return data;
   });
-  
 };
 
-
-const bombay = breedDetailsFromFile('Bombay');
-
-console.log('Return Value: ', bombay); 
+const bombay = breedDetailsFromFile("Bombay");
+//this line needs to be moved into the callback function.
+//console.log("Return Value: ", bombay);
